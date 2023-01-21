c71b505ff25e02559dd933a00ac87e9e36fef8ca
nthokiesther09 <nthokiesther09@gmail.com>
2023-01-21 21:33:10 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-
+import GifListContainer from "./GifListContainer";
 import NavBar from "./NavBar";
 
 // the App component should render out the GifListContainer component
 
 function App() {
+
+const url = "https://api.giphy.com/v1/gifs/search?q=dolphin"
+const apiKey = "m5iqlYKuFW45Un5yNw0NyBu1nTJwcraF"
+
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <GifListContainer url = {url} apiKey = {apiKey}/>
     </div>
   );
 }
